cf94425646862f0ddb83ecf1dba3bb13624e2c5a
Michael Meow <dearmichaelmeow@gmail.com>
2018-08-17 15:18:15 -0700

    add my own loop function and fix answer for input 0

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,26 @@
 
 // Original assignment
 
-function firstName(name){
-  nameArray = name.split(" ");
-  return nameArray [0];
+// I originally used the .includes method, and then I realized you wanted us to show our knowledge of loops, so I created my own includes function
+function includes(string, search){
+  var characters = string.split("");
+  for (i=0; i<characters.length; i++);
+  var character = characters[i];
+  if (character == search){
+    return "true";
+  }
 }
 
 function result(number, name){
-  if (number.includes("3")){
+  if (includes(number, "3")){
     if (name){
       return "I\'m sorry, " + name + ". I\'m afraid I can\'t do that.";
     } else {
       return "I\'m sorry, Dave. I\'m afraid I can\'t do that.";
     }
-  } else if (number.includes("1")){
+  } else if (includes(number, "1")){
     return "Boop!";
-  } else if (number.includes("0")){
+  } else if (includes(number,"0")){
     return "Beep!";
   } else {
     if(name){
@@ -27,12 +32,19 @@ function result(number, name){
   }
 }
 
-// I finished very early, and my program is clearer and more efficient without any loops (I'm guessing the assignment was designed for us to loop through the numbers in the input in order to find our 0's, 1's and 3's?  The .includes method is much simpler and allows priority for 3 over 1 over 0 a lot easier.)  So I decided to try the binary and hexidecimal translator, where I use loops and show I have understood the concepts.
+function firstName(name){
+  nameArray = name.split(" ");
+  return nameArray [0];
+}
+
+// I finished very early, and I've decided to build a binary and hexidecimal converter as per further exploration assignment.
 
 // Binary translator - proud of this one
 
 function binaryConverter(number){
-  if(number){
+  if (number == 0){
+    return "0";
+  }else if(number){
     var binaryLog = Math.floor(Math.log2(number));
     var binaryArray = new Array(binaryLog).fill(0);
     while(number>0){
@@ -49,37 +61,40 @@ function binaryConverter(number){
 // Now to hexidecimal
 
 function hexConverter(number){
-  var binaryArray = binaryConverter(number).split("");
-  var addZeroes = 4 - (binaryArray.length % 4);
-  var hexDigitBinary = [];
-  var hexDigitDecimal = 0;
-  var decimals = [];
-  for (;addZeroes>0; addZeroes -= 1){
-    binaryArray.unshift(0);
-  }
-  // this converts every four digits of the binary to a decimal value under 16
-  for (i=4;i<=binaryArray.length; i+=4){
-    hexDigitBinary = binaryArray.slice(i-4,i);
-    for (j=0; j<4; j++){
-      console.log(hexDigitBinary[j]);
-      if (hexDigitBinary[j] == 1) {
-        hexDigitDecimal += Math.pow(2, (4-(j+1)));
+  if (number == 0){
+    return "0";
+  }else if(number){
+    var binaryArray = binaryConverter(number).split("");
+    var addZeroes = 4 - (binaryArray.length % 4);
+    var hexDigitBinary = [];
+    var hexDigitDecimal = 0;
+    var decimals = [];
+    for (;addZeroes>0; addZeroes -= 1){
+      binaryArray.unshift(0);
+    }
+    // this converts every four digits of the binary to a decimal value under 16
+    for (i=4;i<=binaryArray.length; i+=4){
+      hexDigitBinary = binaryArray.slice(i-4,i);
+      for (j=0; j<4; j++){
+        if (hexDigitBinary[j] == 1) {
+          hexDigitDecimal += Math.pow(2, (4-(j+1)));
+        }
       }
+      decimals.push(hexDigitDecimal);
+      hexDigitDecimal = 0;
     }
-    decimals.push(hexDigitDecimal);
-    hexDigitDecimal = 0;
+    // this converts the decimal value into hexidecimal
+    var hex = decimals.map(function(decimal){
+      var letters = ["A", "B", "C", "D", "E", "F"];
+      if (decimal > 9){
+        return letters[decimal - 10];
+      } else {
+        return decimal;
+      }
+    })
+    var result = hex.join("");
+    return result;
   }
-  // this converts the decimal value into hexidecimal
-  var hex = decimals.map(function(decimal){
-    var letters = ["A", "B", "C", "D", "E", "F"];
-    if (decimal > 9){
-      return letters[decimal - 10];
-    } else {
-      return decimal;
-    }
-  })
-  var result = hex.join("");
-  return result;
 }
 
 // User Interface
