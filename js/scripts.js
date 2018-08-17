237f9cdef4b868331c59e3d36e507b2e1b4ccc4f
Michael Meow <dearmichaelmeow@gmail.com>
2018-08-17 09:09:39 -0700

    meet fifth BDD expectation and return hal's message when input has a 3

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,9 @@ $(function(){
     event.preventDefault();
       var numInput = $("#numInput").val();
       var result = ""
-      if (numInput.includes("1")){
+      if (numInput.includes("3")){
+        result = "I\'m sorry, Dave. I\'m afraid I can\'t do that."
+      } else if (numInput.includes("1")){
         result = "Boop!"
       } else if (numInput.includes("0")){
         result = "Beep!"
