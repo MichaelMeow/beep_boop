16ae2c55aeee20dd4c0fe4d0b4720b8eef036550
Michael Meow <dearmichaelmeow@gmail.com>
2018-08-17 10:19:35 -0700

    add feature to only use first name in message

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 // Business Logic
-
+function firstName(name){
+  nameArray = name.split(" ")
+  return nameArray [0]
+}
 // Method .includes contains the business logic I needed to meet the expectations
 
 // User Interface
@@ -8,9 +11,13 @@ $(function(){
     event.preventDefault();
       var numInput = $("#numInput").val();
       var result = ""
-      var userName = $("#userName").val();
+      var userName = firstName($("#userName").val());
       if (numInput.includes("3")){
+        if (userName){
         result = "I\'m sorry, " + userName + ". I\'m afraid I can\'t do that."
+        } else {
+        result = "I\'m sorry, Dave. I\'m afraid I can\'t do that."
+        }
       } else if (numInput.includes("1")){
         result = "Boop!"
       } else if (numInput.includes("0")){
