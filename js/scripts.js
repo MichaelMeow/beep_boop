0236abe7655843a3c6995a4804cae7f743b79f93
Michael Meow <dearmichaelmeow@gmail.com>
2018-08-17 09:13:36 -0700

    add a name input and change dave to user name

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,9 @@ $(function(){
     event.preventDefault();
       var numInput = $("#numInput").val();
       var result = ""
+      var userName = $("#userName").val();
       if (numInput.includes("3")){
-        result = "I\'m sorry, Dave. I\'m afraid I can\'t do that."
+        result = "I\'m sorry, " + userName + ". I\'m afraid I can\'t do that."
       } else if (numInput.includes("1")){
         result = "Boop!"
       } else if (numInput.includes("0")){
